119a7358b7b076ea7bc9e784f845cb65e8e11bd1
Tao He <sighingnow@gmail.com>
2016-12-18 22:36:38 +0800

    Improve tests for Python 3, for issue #9.

--- tests/python_3_only/test_generator.py
+++ tests/python_3_only/test_generator.py
@@ -12,27 +12,35 @@ import unittest
 from parsec import *
 
 class ParserGeneratorWithReturnTest(unittest.TestCase):
-    '''Test the implementation of Parser Generator with 'return' statement.
+    '''
+    Test the implementation of Parser Generator with 'return' statement.
+    Since 'return' with argument inside generator is not supported in Python 2, this usage
+    is Python 3 only. For Python 2 users, you can raise a exception to pass return value.
+    More detailed discussion about this topic could be found at
+    `https://github.com/sighingnow/parsec.py/issues/9 <https://github.com/sighingnow/parsec.py/issues/9>`_ .
     '''
     def test_generate(self):
-        nonlocals = {'x': None, 'y': None}
+        x, y = None, None
+
         @generate
         def fn():
-            nonlocals['x'] = yield string('x')
-            nonlocals['y'] = yield string('y')
+            nonlocal x, y
+            x = yield string('x')
+            y = yield string('y')
             return string('z')
         self.assertEqual(fn.parse('xyz'), 'z')
-        self.assertEqual(nonlocals['x'], 'x')
-        self.assertEqual(nonlocals['y'], 'y')
+        self.assertEqual(x, 'x')
+        self.assertEqual(y, 'y')
 
-        nonlocals = {'x': None, 'y': None}
         @generate
         def fn():
-            nonlocals['x'] = yield digit()
-            nonlocals['y'] = yield count(digit(), 5)
+            nonlocal x, y
+            x = yield digit()
+            y = yield count(digit(), 5)
+            return None
         self.assertEqual(fn.parse('123456'), None)
-        self.assertEqual(nonlocals['x'], '1')
-        self.assertEqual(nonlocals['y'], ['2', '3', '4', '5', '6'])
+        self.assertEqual(x, '1')
+        self.assertEqual(y, ['2', '3', '4', '5', '6'])
 
 if __name__ == '__main__':
     unittest.main()
