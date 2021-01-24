057e0a9a34ea6d713db55261456aa01bba18177b
Tao He <sighingnow@gmail.com>
2021-01-24 14:16:25 +0800

    Bump the version to v3.6.
    Signed-off-by: Tao He <sighingnow@gmail.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'parsec',
-    version = '3.5',
+    version = '3.6',
     description = 'parser combinator.',
     long_description = 'A universal Python parser combinator library inspired by Parsec library of Haskell.',
     author = 'He Tao',
@@ -29,6 +29,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         "License :: OSI Approved :: MIT License",
     ],
     platforms = 'any',
